74eed61cd38569c889008d806d915aa8bd7b8a2b
重彦 <meiguanghui.mgh@alibaba-inc.com>
2022-11-13 21:42:13 +0800

    exec log test

--- exec-hook.c
+++ exec-hook.c
@@ -9,7 +9,7 @@ typedef ssize_t (*execve_func_t)(const char *filename, char *const argv[], char 
 static execve_func_t old_execve = NULL;
 
 int execve(const char *filename, char *const argv[], char *const envp[]) {
-    printf("Running hook\n");
+    printf("[EXEC_HOOK] filename: %s, args: %s, env: %s", filename, argv, envp);
     old_execve = dlsym(RTLD_NEXT, "execve");
     return old_execve(filename, argv, envp);
 }
