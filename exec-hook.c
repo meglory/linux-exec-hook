fa4642aa711de78edd498749ba80290bd365387e
重彦 <meiguanghui.mgh@alibaba-inc.com>
2022-11-14 13:39:28 +0800

    debug

--- exec-hook.c
+++ exec-hook.c
@@ -11,21 +11,21 @@ static execve_func_t old_execve = NULL;
 
 int execve(const char *filename, char *const argv[], char *const envp[]) {
     printf("\n[EXEC_HOOK] filename: %s, args: ", filename);
-//    for (size_t i = 0; argv[i] != NULL; ++i) {
-//        printf("%s ", argv[i]);
-//    }
-//    printf(", envs: ");
-//    for (size_t i = 0; envp[i] != NULL; ++i) {
-//        printf("%s ", envp[i]);
-//    }
+    for (size_t i = 0; argv[i] != NULL; ++i) {
+        printf("%s ", argv[i]);
+    }
+    printf(", envs: ");
+    for (size_t i = 0; envp[i] != NULL; ++i) {
+        printf("%s ", envp[i]);
+    }
     printf("\n");
     printf("[EXEC_HOOK] before.");
-//    int ret = strcmp(argv[0], "java");
+    int ret = strcmp(argv[0], "java");
     printf("[EXEC_HOOK] after.");
-//    printf("[EXEC_HOOK] argv0: %s, check result: %d", argv[0], ret);
-//    if (ret == 0) {
-//        printf("[EXEC_HOOK] got java process");
-//    }
+    printf("[EXEC_HOOK] argv0: %s, check result: %d", argv[0], ret);
+    if (ret == 0) {
+        printf("[EXEC_HOOK] got java process");
+    }
     old_execve = dlsym(RTLD_NEXT, "execve");
     return old_execve(filename, argv, envp);
 }
