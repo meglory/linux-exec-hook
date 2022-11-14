1679d5e02d852436cc2c555e16d9f572d6aea1b2
重彦 <meiguanghui.mgh@alibaba-inc.com>
2022-11-14 13:29:15 +0800

    debug

--- exec-hook.c
+++ exec-hook.c
@@ -22,10 +22,10 @@ int execve(const char *filename, char *const argv[], char *const envp[]) {
     printf("[EXEC_HOOK] before.");
 //    int ret = strcmp(argv[0], "java");
     printf("[EXEC_HOOK] after.");
-    printf("[EXEC_HOOK] argv0: %s, check result: %d", argv[0], ret);
-    if (ret == 0) {
-        printf("[EXEC_HOOK] got java process");
-    }
+//    printf("[EXEC_HOOK] argv0: %s, check result: %d", argv[0], ret);
+//    if (ret == 0) {
+//        printf("[EXEC_HOOK] got java process");
+//    }
     old_execve = dlsym(RTLD_NEXT, "execve");
     return old_execve(filename, argv, envp);
 }
