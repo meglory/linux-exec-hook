8f16e5adf3fb7f1b8bbdc8b17a0a9db19ade15ef
重彦 <meiguanghui.mgh@alibaba-inc.com>
2022-11-14 13:16:01 +0800

    debug

--- exec-hook.c
+++ exec-hook.c
@@ -19,6 +19,7 @@ int execve(const char *filename, char *const argv[], char *const envp[]) {
         printf("%s ", envp[i]);
     }
     printf("\n");
+    printf("[EXEC_HOOK] before.");
     int ret = strcmp(argv[0], "java");
     printf("[EXEC_HOOK] after.");
     printf("[EXEC_HOOK] argv0: %s, check result: %d", argv[0], ret);
