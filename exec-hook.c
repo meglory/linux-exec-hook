525c560812c72ee531c241847f01ab39cf71f498
重彦 <meiguanghui.mgh@alibaba-inc.com>
2022-11-14 11:16:37 +0800

    exec log test

--- exec-hook.c
+++ exec-hook.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <dlfcn.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef ssize_t (*execve_func_t)(const char *filename, char *const argv[], char *const envp[]);
 
@@ -10,14 +11,17 @@ static execve_func_t old_execve = NULL;
 
 int execve(const char *filename, char *const argv[], char *const envp[]) {
     printf("[EXEC_HOOK] filename: %s, args: ", filename);
-    for (char *i = argv; *i != NULL; ++i) {
-        printf("%s ", *i);
+    for (size_t i = 0; argv[i] != NULL; ++i) {
+        printf("%s ", argv[i]);
     }
     printf(", envs: ");
-    for (char *i = envp; *i != NULL; ++i) {
-        printf("%s ", *i);
+    for (size_t i = 0; envp[i] != NULL; ++i) {
+        printf("%s ", envp[i]);
     }
     printf("\n");
+    if(strcmp(argv[0], "java") == 0) {
+        printf("[EXEC_HOOK] got java process", filename);
+    }
     old_execve = dlsym(RTLD_NEXT, "execve");
     return old_execve(filename, argv, envp);
 }
